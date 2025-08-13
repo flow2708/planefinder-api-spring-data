89e25839c15aa40c81a174c56ef1c97176c8660f
flow2708 <dibirovamin10@gmail.com>
2025-08-13 23:56:47 +0300

    Initial commit

---
+++ src/main/java/com/example/planefinder_api/PlanefinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.planefinder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanefinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanefinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/planefinder_api/PlanefinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planefinder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanefinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
